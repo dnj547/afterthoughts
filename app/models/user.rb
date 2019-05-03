e354650a7b056e91e594036cfa69754b2c0fa602
danfyfe <fyfedaniel@gmail.com>
2019-05-03 13:52:04 -0400

    seeds and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :calendars
   has_many :events, through: :calendars
+  has_many :afterthoughts, through: :calendars
+  has_secure_password
+
+  validates :username, uniqueness: true, length: {maximum: 30}
+
 end
