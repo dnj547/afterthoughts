ff3ab4c09073dd07810b7ba5abc9fdadd30f8211
dnj547 <dnj547@live.com>
2019-05-03 14:42:07 -0400

    Create more seeds, test seed data, edit validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,18 +4,17 @@ class Event < ApplicationRecord
   has_many :event_attendees
   has_many :attendees, through: :event_attendees
 
-  
+
   validates :organizer, presence: true, length: {maximum: 30}
   validates :title, presence: true, length: {maximum: 30}
-  # validates :description, presence: true
   validates :start, presence: true
   validates :end, presence: true
-  # validates :location, presence: true, length: {maximum: 30}
   validate :validates_start
 
   def validates_start
+    return if self.end.blank? || self.start.blank?
     if self.start > self.end
-      errors.add(:start, "must be before end time")
+      errors.add(:end, "date/time must be after start date/time")
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ User.destroy_all
 Calendar.destroy_all
 Event.destroy_all
 Afterthought.destroy_all
+Attendee.destroy_all
+EventAttendee.destroy_all
+AfterthoughtAttendee.destroy_all
 
 
 ##User Seeds##
@@ -11,31 +14,50 @@ rob = User.create!(username:"RobOGara", password_digest: "password")#3
 jeff = User.create!(username:"JeffMarks", password_digest: "password")#4
 
 ##Calendar Seeds##
-calendar1 = Calendar.create!(user:dan,name:"Social")#1
-Calendar.create!(user: dan,name:"Work")#2
-Calendar.create!(user: danielle,name:"Social")#3
-Calendar.create!(user: danielle,name:"Work")#4
-Calendar.create!(user: rob,name:"Social")#5
-Calendar.create!(user: rob,name:"Work")#6
-Calendar.create!(user: jeff,name:"Social")#7
-Calendar.create!(user: jeff,name:"Work")#8
+c1 = Calendar.create!(user:dan,name:"Social")#1
+c2 = Calendar.create!(user: dan,name:"Work")#2
+c3 = Calendar.create!(user: danielle,name:"Social")#3
+c4 = Calendar.create!(user: danielle,name:"Work")#4
+c5 = Calendar.create!(user: rob,name:"Social")#5
+c6 = Calendar.create!(user: rob,name:"Work")#6
+c7 = Calendar.create!(user: jeff,name:"Social")#7
+c8 = Calendar.create!(user: jeff,name:"Work")#8
 
 #DateTime Seeds##
-may05noon = DateTime.new(2019,05,05,12)
-may05five = DateTime.new(2019,05,05,17)
+may05noon = DateTime.new(2019,5,5,12)
+may05fivepm = DateTime.new(2019,5,5,17)
+apr08onepm = DateTime.new(2019,4,8,13)
+apr08twopm = DateTime.new(2019,4,8,14)
+apr12onepm = DateTime.new(2019,4,12,13)
+march01nineam = DateTime.new(2019,3,1,9)
+march01tenam = DateTime.new(2019,3,1,10)
 
 
-##Events Seeds##
- event1 = Event.create!(calendar: calendar1,visibility:"public",organizer:"Mom", title:"Sunday Dinner",start:may05noon,end:may05five,location:"Home")
+##Event Seeds##
+e1 = Event.create!(calendar:c1,visibility:"public",organizer:"Mom", title:"Sunday Dinner",start:may05noon,end:may05fivepm,location:"Home")
+e2 = Event.create!(calendar:c1,visibility:"public",organizer:"Dad",title:"game night",start:apr08onepm,end:apr12onepm,location:"Home")
+e3 = Event.create!(calendar:c1,visibility:"public",organizer:"Dad",title:"game night",start:apr08onepm,end:apr12onepm,location:"Home")
 
 
 ##Afterthought Seeds##
-Afterthought.create!(actual_location: "Home", actual_start: may05noon, actual_end: may05five, actual_description: "It really was Sunday Dinner", thoughts:"It was great",rating:5, event: event1)
+at1 = Afterthought.create!(actual_location: "Home", actual_start: may05noon, actual_end: may05fivepm, actual_description: "It really was Sunday Dinner", thoughts:"It was great",rating:5, event: e1)
+at2 = Afterthought.create!(actual_location: "Home", actual_start: apr08twopm, actual_end: apr12onepm, actual_description: "a fun game night", thoughts:"good",rating:4, event: e2)
 
 
 ##Attendee Seeds##
-Attendee.create!(name:"Buddy",notes:"Kind of a jerk")
-Attendee.create!(name:"The Other One",notes:"Blah Blah")
+att1 = Attendee.create!(name:"Buddy",notes:"Kind of a jerk")
+att2 = Attendee.create!(name:"The Other One",notes:"Blah Blah")
+att3 = Attendee.create!(name:"Dad",notes:"ugh")
+att4 = Attendee.create!(name:"Mom",notes:"ughhhh")
 
+##EventAttendee Seeds##
+ea1 = EventAttendee.create!(event: e1, attendee: att1)
+ea2 = EventAttendee.create!(event: e1, attendee: att2)
+ea3 = EventAttendee.create!(event: e2, attendee: att3)
+ea3 = EventAttendee.create!(event: e3, attendee: att3)
 
-
+##AfterthoughtAttendee Seeds##
+aa1 = AfterthoughtAttendee.create!(afterthought: at1, attendee: att1)
+aa2 = AfterthoughtAttendee.create!(afterthought: at1, attendee: att4)
+aa3 = AfterthoughtAttendee.create!(afterthought: at2, attendee: att3)
+aa4 = AfterthoughtAttendee.create!(afterthought: at2, attendee: att1)
