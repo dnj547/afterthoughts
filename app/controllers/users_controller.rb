b1ea685b1e11ddb85f7e6bb5b48096897cfade00
danfyfe <fyfedaniel@gmail.com>
2019-05-03 16:43:12 -0400

    controllers and sessions start

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password,:password_confirmation)
+  end
+
+
+
+end
---
+++ app/helpers/afterthoughts_helper.rb
@@ -0,0 +1,2 @@
+module AfterthoughtsHelper
+end
