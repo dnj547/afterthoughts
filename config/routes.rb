8e9cfc8011fdaf18b5a5744837501b88b8b1573f
danfyfe <fyfedaniel@gmail.com>
2019-05-06 16:04:06 -0400

    forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
 
   # get 'users/signup', to: 'users#new', as: 'signup'
 
-  resources :users, :events, :afterthoughts
-
+  resources :users, :afterthoughts
+  resources :events
   resources :sessions, only: [:new,:create,:destroy]
+
+  get "/events/new/:calendar_id/:year/:month/:day", to: 'events#new'
+
+  ## signup/login/logout routes ##
   get 'signup', to:'users#new', as:'signup'
   get 'login', to: 'sessions#new', as:'login'
   get 'logout', to:'sessions#destroy', as: 'logout'
+
+
 end
