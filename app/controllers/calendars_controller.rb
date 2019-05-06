ea6e2a148ee23781032237dde0dbc5c3f9376924
dnj547 <dnj547@live.com>
2019-05-06 18:04:18 -0400

    Calendar edit form and actions

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,19 @@ class CalendarsController < ApplicationController
     redirect_to '/users/:id'
   end
 
+  def edit
+    @calendar = Calendar.find(params[:id])
+  end
+
+  def update
+    @calendar = Calendar.find(params[:id])
+    if @calendar.update(calendar_params)
+     redirect_to calendar_path(@calendar)
+   else
+     render :edit
+   end
+  end
+
   private
   def calendar_params
     params.require(:calendar).permit(:user_id, :name)
