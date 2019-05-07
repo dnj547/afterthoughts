dda872811ea0d1c38a2bdcdeb9fb41b538111159
dnj547 <dnj547@live.com>
2019-05-07 16:48:59 -0400

    Fix user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
