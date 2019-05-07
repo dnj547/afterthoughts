d078a63c002eb4fd7386a9cd3730eb2713105396
dnj547 <dnj547@live.com>
2019-05-07 14:30:11 -0400

    Resolve merge conflicts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,20 +8,14 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
-      # redirect_to '/profile'
     else
       redirect_to new_session_path
-      redirect_to '/login'
     end
   end
 
   def destroy
-    # session[:user_id] = nil
-    session.delete(:user)
+    session[:user_id] = nil
     redirect_to root_url, notice:"Logged out!"
   end
-
-
-
 
 end
