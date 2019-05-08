ef24e6df7c576464d1f3056ff3b98daae21f9856
dnj547 <dnj547@live.com>
2019-05-08 17:10:07 -0400

    Add attendee to event

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,13 +1,25 @@
 class AttendeesController < ApplicationController
 
-  def index
-    byebug
+  def new
+    @attendee = Attendee.new
+    @event = Event.last
   end
 
   def create
-    byebug
-
+    @user = current_user
+    @event = Event.last
+    @attendee = Attendee.create(attendee_params)
+    @event_attendee = EventAttendee.create(event: @event, attendee: @attendee)
+    if @attendee.valid?
+      flash[:message] = "Event Created"
+      redirect_to @user
+    end
   end
 
+  private
+
+  def attendee_params
+    params.require(:attendee).permit(:name, :notes)
+  end
 
 end
