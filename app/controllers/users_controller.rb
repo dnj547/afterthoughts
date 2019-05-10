dd30b733fb4e4ff595c2e9431b0e28a303d2eb1e
dnj547 <dnj547@live.com>
2019-05-10 12:43:29 -0400

    Fix more errors and update buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show]
   before_action :correct_user,   only: [:show]
 
-
   def show
     @user = current_user
     @calendars = @user.calendars
@@ -37,7 +36,7 @@ class UsersController < ApplicationController
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in."
+      flash[:alert] = "Please log in."
       redirect_to login_url
     end
   end
